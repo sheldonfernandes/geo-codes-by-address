642fa24e54ecf2ee139ef377430d8a146de887ea
Sheldon Fernandes <sheldon3@us.ibm.com>
2023-03-26 22:36:20 -0400

    goe code finder utility

---
+++ README.md
@@ -0,0 +1,13 @@
+ ## Gets geo codes for addresses
+
+This programme gets GeoCodes for all the addresses (North Carolina only)) in the CSV and writes it to an output csv file 
+
+## Requirements
+
+Install the required libraries:
+
+	$ pip install -r requirements.txt
+
+After that run with:
+
+	$ python findAddress.py <inputcsv> <outputcsv>
---
+++ findAddress.py
@@ -0,0 +1,62 @@
+import requests
+import sys
+import csv
+
+# gather inputs
+try:
+    input = sys.argv[1]
+    output = sys.argv[2]
+except IndexError:
+    raise SystemExit(f"Usage: {sys.argv[0]} <csv-file> <output-file>")
+
+
+# Define the endpoint URL for the geocoding API
+# url = 'https://services.nconemap.gov/secure/rest/services/Geocoding/GeocodeServer/findAddressCandidates'
+url = 'https://services.nconemap.gov/secure/rest/services/AddressNC/AddressNC_geocoder/GeocodeServer/findAddressCandidates'
+
+# Define the parameters for the geocoding request
+params = {
+    'f': 'json',                # Response format
+    'outFields': '*',           # Return all fields in the response
+    'maxLocations': 5,          # Maximum number of results to return
+    # 'SingleLine': '101 Rose Hill dr, Holly Springs',  # Address to geocode
+    'outSR': '4326'            # Spatial reference for the output
+    #'apiKey': 'YOUR_API_KEY'    # Your API key (replace with actual API key)
+}
+
+
+with open(input) as inputCSV:
+    with open(output, 'w') as outputCSV:
+        csv_reader = csv.reader(inputCSV, delimiter=',')
+        writer = csv.writer(outputCSV, lineterminator='\n')
+        line_count = 0
+
+        for row in csv_reader:
+            # set new x and y headers for the output csv
+            if line_count == 0:
+                writer.writerow(row+['x']+['y'])
+
+            if row and line_count > 0:
+                params['SingleLine'] = row[0] + ' ' + row[2] + ' ' + row[3];
+                
+                # Send the request to the API and get the response
+                response = requests.get(url, params=params)
+
+                # Check if the request was successful (HTTP status code 200)
+                if response.status_code == 200:
+                    # Parse the JSON response
+                    data = response.json()
+                    
+                    # Save results to output CSV
+                    if data['candidates']:
+                        for candidate in data['candidates']:
+                            print(candidate['address'], candidate['location']['x'], candidate['location']['y'])
+                            writer.writerow(row+[candidate['location']['x']]+[candidate['location']['y']])
+                    else:
+                        print(row[0] + ' *** No geo code found ***')
+                        writer.writerow(row)
+
+                else:
+                    print('Error:', response.status_code)
+            line_count += 1
+    print(f'Complete. {line_count} lines processed')
